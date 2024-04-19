b3678bbc893999e1ace2a2a311247f46bd5fd477
Neel Kumar <neelansh2013@gmail.com>
2024-04-19 10:51:39 -0500

    Grants

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template
+import mysql.connector
+
+
+
  
 app = Flask(__name__)
+sqlconnection = ""
+
+
+
+
+
+
 
 @app.route('/grantize')
 def grantize():
@@ -50,8 +61,116 @@ def termsnconditions():
 def grantizegrants():
     return render_template('grantize/grants/grants.html')
 
+@app.route('/registerresgrantize', methods =["GET", "POST"])
+def registerresgrantize():
+    global sqlconnection
+    print("ENTER REGISTER FUNCTION")
+    if "_tokenpersonal" in request.form:
+        try:
+            name_prefix = request.form['name_prefix']
+            fname = request.form['fname']
+            lname = request.form['lname']
+            qualification = request.form['qualification']
+            email = request.form['email_personal']
+            mobile = request.form['mobile']
+            username = request.form['uname']
+            password = request.form['password']
+            addl1 = request.form['address_line_one']
+            addl2 = request.form['address_line_two']
+            postcode = request.form['post_code']
+            country = request.form['country']
+            state = request.form['state']
+            city = request.form['city']
+        except:
+            print("REGISTER USER VARIABLES COULD NOT BE READ!!")
+            return render_template('grantize/grantize.html')
+        mycursor = sqlconnection.cursor()
+        sql = "INSERT INTO gresearcherslist (prefix, firstname, lastname, qualification, email, mobile, username, password, addressline1, addressline2, postcode, country, state, city) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (name_prefix, fname, lname, qualification, email, mobile, username, password, addl1, addl2, postcode, country, state, city)
+        try:
+            mycursor.execute(sql, val)
+            sqlconnection.commit()
+            mycursor.close()
+            print("Database Insertion Successful....")
+        except:
+            print("Database Insertion Failed!!")
+        return render_template('grantize/dashboard/dashboard.html')
+    elif "_tokenmanager" in request.form:
+        try:
+            organization = request.form['organization']
+            email_prefix = request.form['email_prefix']
+            email_domain = request.form['email_domain']
+            email_institutional = request.form['email_institutional']
+            fname = request.form['fname']
+            lname = request.form['lname']
+            email_personal = request.form['email_personal']
+            mobile = request.form['mobile']
+            uname = request.form['uname']
+            password = request.form['password']
+            address_line_one = request.form['address_line_one']
+            address_line_two = request.form['address_line_two']
+            post_code = request.form['post_code']
+            country = request.form['country']
+            state = request.form['state']
+            city = request.form['city']
+        except:
+            print("REGISTER USER VARIABLES COULD NOT BE READ!!")
+            return render_template('grantize/grantize.html')
+        mycursor = sqlconnection.cursor()
+        sql = "INSERT INTO gmanagerslist (organization, email_prefix, email_domain, email_institutional, fname, lname, email_personal, mobile, uname, password, address_line_one, address_line_two, postcode, country, state, city) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (organization, email_prefix, email_domain, email_institutional, fname, lname, email_personal, mobile, uname, password, address_line_one, address_line_two, post_code, country, state, city)
+        try:
+            mycursor.execute(sql, val)
+            sqlconnection.commit()
+            mycursor.close()
+            print("Database Insertion Successful....")
+        except:
+            print("Database Insertion Failed!!")
+        return render_template('grantize/dashboard/dashboard.html')
+    else:
+        return render_template('grantize/dashboard/dashboard.html')
+
+
 @app.route('/logingrantizeresdash', methods =["GET", "POST"])
 def logingrantizeresdash():
+    global sqlconnection
+    print("ENTER REGISTER FUNCTION")
+    if "_tokenresearcher" in request.form:
+        try:
+            username = request.form['user_name']
+            password = request.form['pass_word']
+        except:
+            print("REGISTER USER VARIABLES COULD NOT BE READ!!")
+            return render_template('grantize/grantize.html')
+        mycursor = sqlconnection.cursor()
+        sql = "SELECT * FROM gmanagerslist WHERE uname = %s AND password = %s"
+        values = (username, password)
+        mycursor.execute(sql, values)
+        result = mycursor.fetchall()
+        if result:
+            print("Match Found..")
+            return render_template('grantize/dashboard/dashboard.html')
+        else:
+            print("Match Not Found..")
+            return render_template('grantize/grantize.html')
+    elif "_tokensponsor" in request.form:
+        try:
+            username = request.form['user_name']
+            password = request.form['pass_word']
+        except:
+            print("REGISTER USER VARIABLES COULD NOT BE READ!!")
+            return render_template('grantize/grantize.html')
+        mycursor = sqlconnection.cursor()
+        sql = "SELECT * FROM gsponsorslist WHERE username = %s AND password = %s"
+        values = (username, password)
+        mycursor.execute(sql, values)
+        result = mycursor.fetchall()
+        if result:
+            print("Match Found..")
+            return render_template('grantize/dashboard/dashboard.html')
+        else:
+            print("Match Not Found..")
+            return render_template('grantize/grantize.html')
     return render_template('grantize/dashboard/dashboard.html')
 
 @app.route('/grantizeprofile')
@@ -64,7 +183,32 @@ def grantizerefreq():
 
 @app.route('/grantizebrowsegrants')
 def grantizebrowsegrants():
-    return render_template('grantize/dashboard/browsegrants.html')
+    global sqlconnection
+    try:
+        mycursor = sqlconnection.cursor()
+        sql = "SELECT id,title,grants_type,subjects FROM ggrants"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(result[:1])
+        return render_template('grantize/dashboard/browsegrants.html', grants=result)
+    except:
+        print("Database Connection Not Working!!")
+        return render_template('grantize/dashboard/dashboard.html')
+    
+@app.route('/grantizeviewgrants', methods =["GET", "POST"])
+def grantizeviewgrants():
+    global sqlconnection
+    grantid = request.form['grantid']
+    print("--------")
+    print(grantid)
+    mycursor = sqlconnection.cursor()
+    sql = "SELECT id,title,subjects,description,submission_info,amount_info,cost_sharing,data_management,contacts,countries,citizenships,grants_type,applicant_types,activity_code,grant_status,open_date,intent_due_date,application_due_date,earliest_start_date,expiration_date,currency,amount_per_grant_min,amount_per_grant_max,award_min,award_max,cfda,grant_source_url FROM ggrants WHERE id = %s"
+    values = (grantid,)
+    mycursor.execute(sql, values)
+    result = mycursor.fetchall()
+    print(result[0])
+    id,title,subjects,description,submission_info,amount_info,cost_sharing,data_management,contacts,countries,citizenships,grants_type,applicant_types,activity_code,grant_status,open_date,intent_due_date,application_due_date,earliest_start_date,expiration_date,currency,amount_per_grant_min,amount_per_grant_max,award_min,award_max,cfda,grant_source_url = result[0]
+    return render_template('grantize/dashboard/viewgrants.html', id=id, title=title, subjects=subjects, description=description, submission_info=submission_info, amount_info=amount_info, cost_sharing=cost_sharing, data_management=data_management, contacts=contacts, countries=countries, citizenships=citizenships, grants_type=grants_type, applicant_types=applicant_types, activity_code=activity_code, grant_status=grant_status, open_date=open_date, intent_due_date=intent_due_date, application_due_date=application_due_date, earliest_start_date=earliest_start_date, expiration_date=expiration_date, currency=currency, amount_per_grant_min=amount_per_grant_min, amount_per_grant_max=amount_per_grant_max, award_min=award_min, award_max=award_max, cfda=cfda, grant_source_url=grant_source_url)
 
 @app.route('/grantizesearchquery')
 def grantizesearchquery():
@@ -411,7 +555,27 @@ def sislabrecpages():
 
 
 
+
+
+
+
+
+def connect_with_database():
+    global sqlconnection
+    sqlconnection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="grant"
+        )
+    if sqlconnection.is_connected():
+        print("Database Connection Succeeded!!")
+    else:
+        print("Database Connection Failed!!")
+        exit
+
  
 # main driver function
 if __name__ == '__main__':
+    connect_with_database()
     app.run(port = 6991, debug = True)
